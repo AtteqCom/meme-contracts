64eaa2dc6fed913d85af90697091f9f6ff50ea0f
Lubomir Dani <dani@atteq.com>
2021-05-30 06:09:37 -0400

    Refactor use openzeppelin implementation of Ownable, Pausable, AccessControl

--- src/contracts/Memecoin.sol
+++ src/contracts/Memecoin.sol
@@ -3,13 +3,19 @@ pragma solidity 0.8.0;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
 
 
-/// @title ERC20 token
-/// @dev Will be replaced by current MarbleCoin version
-contract Memecoin is Ownable, ERC20 {
+/**
+* @title Memecoin token
+* @dev main ERC20 currency for meme.com contracts
+*/
+contract Memecoin is Ownable, AccessControl, Pausable, ERC20 {
 
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+  bool private _supplycapped = false;
 
   constructor(uint256 _totalSupply, string memory _memeTokenName, string memory _memeTokenSymbol) ERC20(_memeTokenName, _memeTokenSymbol)
   {
@@ -17,6 +23,58 @@ contract Memecoin is Ownable, ERC20 {
     _mint(msg.sender, mtc * _totalSupply);
 
     _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+  }
+
+  /**
+  * @dev Allows address with granted MINTER_ROLE to mint a number of coins
+  * @param _account Address where will minted coins appear
+  * @param _amount Amount of coins to mint
+  */
+  function mint(address _account, uint256 _amount) 
+    public 
+    whenNotPaused
+  {
+    require(hasRole(MINTER_ROLE, msg.sender), "Address is not minter");
+    require(totalSupply() + _amount > totalSupply(), "Increase in supply would cause overflow.");
+    require(!isSupplyCapped(), "Supply has been capped.");
+    
+    _mint(_account, _amount);
+  }
+
+  /**
+  * @dev Allows address to burn a number of coins in its ownership
+  * @param _account Address what desires to burn its tokens
+  * @param _amount Amount of coins to burn
+  */
+  function burn(address _account, uint256 _amount) 
+    public 
+    whenNotPaused
+  {    
+    _burn(_account, _amount);
+  }
+
+  /**
+  * @dev The contract owner can prevent the minting of new coins
+  * !!!!! This is a one-way function. Once the supply is capped it can't be uncapped.
+  */
+  function freezeMint() 
+    public 
+    onlyOwner 
+    returns (bool) 
+  {
+    _supplycapped = true;
+    return isSupplyCapped();
+  }
+
+  /**
+  * @dev View function to check whether the supply has been capped.
+  */
+  function isSupplyCapped() 
+    public 
+    view 
+    returns (bool) 
+  {
+    return _supplycapped;
   }
 
   /**
@@ -38,7 +96,7 @@ contract Memecoin is Ownable, ERC20 {
     public
     onlyOwner 
   {
-    revokeRole(MINTER_ROLE, oldMTokenFactoryAddress);
+    revokeRole(MINTER_ROLE, _minterAddress);
   }
 
   /**
@@ -50,7 +108,26 @@ contract Memecoin is Ownable, ERC20 {
     view
     returns (bool _isMinter)
   {
-    // remove previouse factory
-    return hasRole(MINTER_ROLE, oldMTokenFactoryAddress);
+    return hasRole(MINTER_ROLE, _minterAddress);
+  }
+
+    /**
+  * @dev Pause contract
+  */
+  function pause()
+    external
+    onlyOwner
+  {
+    _pause();
+  }
+
+  /**
+  * @dev Unpoause contract 
+  */
+  function unpouse()
+    external
+    onlyOwner
+  {
+    _unpause();     
   }
 }
