610e9121ce1d2246c7e1a3cb10f932b29622b3ed
Lubomir Dani <dani@atteq.com>
2021-06-17 08:22:40 -0400

    Update - mTokenRegister is checked towards sender. AccessControl removed. MTokenRegister is provided in constructor. Added option to pasue contract

--- src/contracts/MTokenFactory.sol
+++ src/contracts/MTokenFactory.sol
@@ -2,11 +2,10 @@
 pragma solidity 0.8.0;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
 
-import {MTokenFactoryInterface} from "./interfaces/MTokenFactoryInterface.sol";
 import {MTokenRegisterInterface} from "./interfaces/MTokenRegisterInterface.sol";
+import {MTokenFactoryInterface} from "./interfaces/MTokenFactoryInterface.sol";
 import {IBancorFormula} from "./bancor/IBancorFormula.sol";
 import {Memecoin} from "./Memecoin.sol";
 import {MTokenInitialSetting} from "./MTokenInitialSetting.sol";
@@ -18,70 +17,34 @@ import {MToken} from "./MToken.sol";
  * @title Memetic Token Factory Contract
  * @notice simple contract with purpose to create Memetic Token Contracts
  */
-contract MTokenFactory is Ownable, AccessControl, MTokenFactoryInterface {
-
-  bytes32 public constant MEME_COIN_REGISTER_ROLE = keccak256("MEME_COIN_REGISTER_ROLE");
+contract MTokenFactory is Ownable, Pausable, MTokenFactoryInterface {
 
   string public constant ERROR_CALLER_IS_NOT_MEME_COIN_REGISTER = 'ERROR_CALLER_IS_NOT_MEME_COIN_REGISTER';
-  string public constant ERROR_MEME_COIN_REGISTER_NOT_SET = 'ERROR_MEME_COIN_REGISTER_NOT_SET';
 
   MTokenRegisterInterface public mTokenRegister;
   Memecoin public reserveCurrency;
   MTokenInitialSetting public mTokenInitialSetting;
   IBancorFormula public bancorFormula;
 
-  /**
-  * @dev Event emited when a new MTokenRegister contract is set
-  * @param newMemecoinRegisterAddress Address of the new MTokenRegister address
-  * @param oldMemecoinRegisterAddress Address of the old MTokenRegister address
-  */
-  event MemecoinRegisterChanged(address newMemecoinRegisterAddress, address oldMemecoinRegisterAddress);
-
-  constructor(Memecoin _reserveCurrency, MTokenInitialSetting _mTokenInitialSetting,  IBancorFormula _bancorFormula) {
-    // Grant the contract deployer the default admin role: it will be able
-    // to grant and revoke any roles
-    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-
+  constructor(MTokenRegisterInterface _mTokenRegister, Memecoin _reserveCurrency, MTokenInitialSetting _mTokenInitialSetting,  IBancorFormula _bancorFormula) {
+    mTokenRegister = _mTokenRegister;
     reserveCurrency = _reserveCurrency;
     mTokenInitialSetting = _mTokenInitialSetting;
     bancorFormula = _bancorFormula;
   }
 
   /**
-  * @dev Sets MTokenRegister contract to be able to add created MemeticTokenContract to register. This mth. removes and grants MEME_COIN_REGISTER_ROLE 
-  * @param _memecoinRegister reference to MTokenRegister contract
-  */
-  function setMemecoinRegsiter(MTokenRegisterInterface _memecoinRegister) 
-    public 
-    onlyOwner 
-  {
-    address oldMemecoinAddress = address(mTokenRegister);
-    address newMemecoinAddress = address(_memecoinRegister);
-
-    // remove previouse factory
-    if (hasRole(MEME_COIN_REGISTER_ROLE, oldMemecoinAddress)) {
-      revokeRole(MEME_COIN_REGISTER_ROLE, newMemecoinAddress);
-    }
-
-    grantRole(MEME_COIN_REGISTER_ROLE, newMemecoinAddress);
-    mTokenRegister = _memecoinRegister;
-
-    emit MemecoinRegisterChanged(newMemecoinAddress, oldMemecoinAddress);
-  }
-
-  /**
-  * @dev Allows to caller with granted MEME_COIN_REGISTER_ROLE to create new MemeticToken Contract (MToken) and adds its reference to MTokenRegister. 
-  * Caller should check validity of token name and symbol. Mth. reverts if MTokenRegister contract is not set.
+  * @dev Allows mTokenRegister as caller to create new MemeticToken Contract (MToken). 
   * @param _mTokenName name of new MToken contract
   * @param _mTokenSymbol symbol of new MToken contract
   */
   function createMToken(string calldata _mTokenName, string calldata _mTokenSymbol)
     external
     override
+    whenNotPaused
     returns(address mTokenContract) 
   {
-    require(address(0) != address(mTokenRegister), ERROR_MEME_COIN_REGISTER_NOT_SET);
-    require(hasRole(MEME_COIN_REGISTER_ROLE, msg.sender), ERROR_CALLER_IS_NOT_MEME_COIN_REGISTER);
+    require(msg.sender == address(mTokenRegister), ERROR_CALLER_IS_NOT_MEME_COIN_REGISTER);
   
     MTokenInitialSetting.MTokenSetting memory mTokenSetting = mTokenInitialSetting.getMTokenInitialSetting();
 
@@ -103,5 +66,23 @@ contract MTokenFactory is Ownable, AccessControl, MTokenFactoryInterface {
     return mTokenAddress;
   }
 
+  /**
+  * @dev Pause contract
+  */
+  function pause()
+    external
+    onlyOwner
+  {
+    _pause();
+  }
 
+  /**
+  * @dev Unpoause contract 
+  */
+  function unpause()
+    external
+    onlyOwner
+  {
+    _unpause();     
+  }
 }
